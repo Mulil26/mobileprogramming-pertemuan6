e55a0c618f12ec9a64a4e45c507bb6e3ef70c60d
Mulil26 <ulilabror261203@gmail.com>
2026-05-27 20:33:46 +0700

    updatte

---
+++ lib/pertemuan/pertemuan10.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class SimpleAppBar extends StatelessWidget {
+  const SimpleAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    PageController controller = PageController(
+      initialPage: 0,
+      viewportFraction: 0.8,
+    );
+
+    List<Color> colors = [Colors.red, Colors.blue, Colors.green];
+
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: const Icon(Icons.menu),
+          title: const Text("Pertemuan 10"),
+          centerTitle: true,
+          actions: const [
+            Icon(Icons.search),
+          ],
+          backgroundColor: Colors.red,
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.blue, Colors.lightBlue],
+                begin: Alignment.bottomRight,
+                end: Alignment.topLeft,
+              ),
+            ),
+          ),
+          bottom: const TabBar(
+            indicatorColor: Colors.white,
+            tabs: [
+              Tab(icon: Icon(Icons.apps), text: "Menu"),
+              Tab(icon: Icon(Icons.image), text: "Image"),
+              Tab(icon: Icon(Icons.info), text: "Info"),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            // Tab Menu
+            Center(
+              child: PageView.builder(
+                controller: controller,
+                itemCount: colors.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    color: colors[index],
+                    width: 200,
+                    height: 200,
+                    margin: const EdgeInsets.all(10),
+                  );
+                },
+              ),
+            ),
+            // Tab Image
+            const Center(child: Text("Image")),
+            // Tab Info
+            const Center(child: Text("Info")),
+          ],
+        ),
+      ),
+    );
+  }
+}
